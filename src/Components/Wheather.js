a03d658067f0dcad0e0e3c2c88feba106a8e7958
ManjiCoder <manjicoder@gmail.com>
2022-07-03 21:34:53 +0530

    Weather Done

--- src/Components/Wheather.js
+++ src/Components/Wheather.js
@@ -19,7 +19,8 @@ function Wheather() {
   const [sunriseTime, setSunriseTime] = useState(null);
   const [sunsetTime, setSunsetTime] = useState(null);
   const [status, setStatus] = useState(null);
-  const [temp, setTemp] = useState(9);
+  const [temp, setTemp] = useState(0);
+  const [realTemp, setRealTemp] = useState(0);
   const [latitude, setLatitude] = useState(0);
   const [longitude, setLongitude] = useState(0);
 
@@ -40,11 +41,14 @@ function Wheather() {
     setPressure(res.main.pressure);
     setSunriseTime(String(new Date(res.sys.sunrise).toLocaleString()));
     setSunsetTime(String(new Date(res.sys.sunset).toLocaleString()));
+    setRealTemp(res.main["feels_like"]);
     console.log(url);
   };
   const handleSearch = () => {
-    setSearch(query);
-    console.log(query);
+    let removeSpace = query.replace(/\s+/g, " ").trim();
+    setSearch(removeSpace);
+    // console.log(query);
+    // console.log(removeSpace);
   };
   useEffect(() => {
     fetchApi();
@@ -75,40 +79,68 @@ function Wheather() {
     navigator.geolocation.getCurrentPosition(success, error, options);
   }
   return (
-    <div className="pb-20 flex flex-wrap justify-center  bg-cyan-500 text-white">
-      <main className="px-3 py-4 mt-20 border-2  bg-cyan-600 rounded-md">
+    <div className="py-11 min-h-screen flex flex-wrap justify-center  bg-cyan-500 text-white">
+      <main className="w-[85%] px-3 py-4 border-2  bg-cyan-600 rounded-md">
         <h1 className="my-4 text-2xl text-center">Wheather</h1>
-        <h1 className="my-4 text-xl text-center">
-          {city}, {country}
-        </h1>
-        <h2 className="my-4 text text-center">{date}</h2>
+        {status === 200 ? (
+          <>
+            <h1 className="my-4 text-xl text-center">
+              {city}, {country}
+            </h1>
+            <h2 className="my-4 text text-center text-slate-300">{date}</h2>
+          </>
+        ) : (
+          ""
+        )}
         <form
-          className="px-4 py-2 border-2 flex flex-wrap bg-cyan-700 space-x-3 rounded-lg items-center"
+          className="px-4 py-2 border-2 flex flex-wrap gap-4 bg-cyan-700 space-x-3 rounded-lg items-center justify-between"
           onSubmit={(e) => e.preventDefault()}
         >
-          <input
-            className="border-solid pl-4 py-1 border-2 border-cyan-400 rounded-lg text-black focus:outline-none focus:ring focus:ring-cyan-500 focus-within:border-transparent"
-            type="text"
-            id="text"
-            name="text"
-            placeholder="Search"
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-          />
-          {/* To get Location */}
-          <button className="hover:scale-125" onClick={handleSearch}>
-            <img className="h-5 invert" src={Search} alt="" />
-          </button>
-          <button className="hover:cursor-pointer" onClick={GetLocation}>
-            <img className="h-7 invert" src={Location} alt="" />
-          </button>
-          <button className="text-3xl" onClick={() => setUnit("metric")}>
-            °C
-          </button>
-          <span className="text-3xl mb-2">|</span>
-          <button className="text-3xl" onClick={() => setUnit("kelvin")}>
-            °F
-          </button>
+          <div className="flex">
+            <input
+              className="border-solid pl-4 py-1 border-2 border-cyan-400 rounded-lg text-black focus:outline-none focus:ring focus:ring-cyan-500 focus-within:border-transparent"
+              type="text"
+              id="text"
+              name="text"
+              placeholder="Search"
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+            {/* To get Location */}
+            {query.length === 0 ? (
+              <button
+                disabled
+                className={`${
+                  query.length === 0 ? "opacity-50 cursor-not-allowed" : ""
+                } ml-3 hover:scale-125`}
+                onClick={handleSearch}
+              >
+                <img className="h-6 invert" src={Search} alt="" />
+              </button>
+            ) : (
+              <button
+                className={`${
+                  query.length === 0 ? "opacity-50 cursor-not-allowed" : ""
+                } ml-3 hover:scale-125`}
+                onClick={handleSearch}
+              >
+                <img className="h-6 invert" src={Search} alt="" />
+              </button>
+            )}
+          </div>
+
+          <div className="flex m-0 space-x-5" style={{ margin: 0 }}>
+            <button className="hover:cursor-pointer" onClick={GetLocation}>
+              <img className="h-7 invert" src={Location} alt="" />
+            </button>
+            <button className="text-3xl" onClick={() => setUnit("metric")}>
+              °C
+            </button>
+            <span className="text-3xl mb-2">|</span>
+            <button className="text-3xl" onClick={() => setUnit("kelvin")}>
+              °F
+            </button>
+          </div>
           {/* <div>
             Latitude: {latitude} Longitude: {longitude}
           </div> */}
@@ -116,27 +148,42 @@ function Wheather() {
         {/* {status===200?console.log('city'):console.log('no city')} */}
         {status === 200 ? (
           <>
-            <div className="py-7 capitalize flex flex-wrap px-2 items-center justify-around">
+            <div className="py-7 capitalize flex flex-wrap px-2 items-center justify-between gap-5">
               <div className="text-center">
                 <img className="h-16" src={Img} alt="" />
                 <span>{description}</span>
               </div>
-              <div className="text-5xl">
-                {temp.toFixed()}°{unit === "metric" ? "C" : "F"}
+              <div>
+                <div className="text-4xl">
+                  {temp.toFixed()}°{unit === "metric" ? "C" : "F"}
+                </div>
+                <div className="text-xl">
+                  {city},{country}
+                </div>
+                <div className="text-slate-300">
+                  Feels Likes:{" "}
+                  <span className="text-white text-xl">
+                    {realTemp.toFixed()}°C
+                  </span>
+                </div>
               </div>
-              <div className="flex flex-wrap flex-col text-left">
+              <div
+                className="flex flex-wrap flex-col text-left"
+                style={{ margin: 0 }}
+              >
                 <span>wind: {wind} Kmph </span>
                 <span>Pressure: {pressure} mb</span>
                 <span>Humitidy: {humidity} </span>
               </div>
             </div>
-            <div className="flex flex-wrap justify-between p-3">
-              <div className="flex flex-wrap flex-col items-center">
+
+            <div className="flex flex-wrap justify-around p-3">
+              <div className="flex flex-col items-center">
                 <img className="w-16" src={Sunrise} alt="" />
                 <span>Sunrise</span>
                 <span>{sunriseTime}</span>
               </div>
-              <div className="flex flex-wrap flex-col items-center">
+              <div className="flex flex-col items-center">
                 <img className="w-16" src={Sunset} alt="" />
                 <span>Sunset</span>
                 <span>{sunsetTime}</span>
